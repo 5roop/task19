4a7f32ff3d647dd536cf62b04d2de3fcc2c2d9f2
5roop <rupnikpeter@gmail.com>
2022-11-22 14:32:38 +0100

    NOSIG

--- 003_debug.py
+++ 003_debug.py
@@ -3,5 +3,5 @@ from pathlib import Path
 from utils import fix_component
 
 f = "/home/rupnik/parlamint/SRB/S/ParlaMint-RS_T12Sv9.xml"
-fout = "./3_test.xml"
+fout = "./003_test.xml"
 fix_component(f, fout)
